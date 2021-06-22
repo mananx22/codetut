e807a678743576a80db3d9ea32afc9c3b4433d15
mananx22 <motorollaman27@gmail.com>
2021-06-22 04:59:57 +0000

    razorpay completed

---
+++ 3. Razorpay/volumes/httpd/website/db.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "db";
+$username = "admin";
+$password = "mananx22";
+$db = "apache";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+?>
